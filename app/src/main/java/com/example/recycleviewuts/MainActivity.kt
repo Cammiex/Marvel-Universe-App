148bd23dc808b30c937fb5cceb15d5492fa7389d
Cammiex <aqilr54@gmail.com>
2023-06-14 07:36:30 +0800

    cac

--- app/src/main/java/com/example/recycleviewuts/MainActivity.kt
+++ app/src/main/java/com/example/recycleviewuts/MainActivity.kt
@@ -39,6 +39,5 @@ class MainActivity : AppCompatActivity() {
     val fragmentTransaction = fragmentManager.beginTransaction()
     fragmentTransaction.replace(R.id.frame_layout, fragment)
     fragmentTransaction.commit()
-
     }
 }
